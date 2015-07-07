3537e6170280a1d7878449a537535e6768fb84b4
Emmanuel Awa <eawa@brandeis.edu>
2015-07-06 22:36:19 -0400

    Started the Express server..... Moved out the node_modules to the root folder as that's where node looks for the modules....

--- app.js
+++ app.js
@@ -6,21 +6,20 @@
 /** Require modules **/
 var express = require("express");
 
-/** Initialize Express app object **/
 
-var app = express();
+/** Initialize Express app object **/
+var app = new express();
+
 
 // Server will be browsed at http://localhost:3000
 var root = __dirname,
     port = 3000;
 
 /** Configure express app **/
-
-app.use( express.static( root + "/public" ));
+app.use( express.static( root + "/public" ) );
 
 /** Start server on port 3000 **/
-
 app.listen( port, function listenCallback() {
-    console.log("Express server is listening on port" + port);
-    console.log("To test, browse to http://localhost:" + port);
+    console.log("Express server listening on port " + port);
+    console.log("Launch webpage using http://localhost:" + port);
 });
